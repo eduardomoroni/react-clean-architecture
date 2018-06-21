85a3786e9b75437f2206af3d54359701cef5ea4b
Eduardo Moroni <eduardomoroni@gmail.com>
2018-06-20 21:32:39 -0300

    feat: add correct types

--- core/lib/adapters/redux/counter.ts
+++ core/lib/adapters/redux/counter.ts
@@ -1,31 +1,48 @@
-const INITIAL_STATE = 0;
+import { Counter } from "../../entities";
+import { CounterInteractor } from "../../useCases";
+import { StateType } from "../../frameworks";
+
+type StateSliceType = Counter;
+
+type ActionType = {
+  type: string,
+  qty?: number,
+};
+
+const INITIAL_STATE = new Counter(0);
 const INCREMENT = 'counter/increment';
 const DECREMENT = 'counter/decrement';
 
-export const counterSelector = (state: any): number => state.counter;
+export const counterSelector = (state: StateType): Counter => state.counter;
 
-export const incrementCounterAction = (qty: number): any => ({
+export const incrementCounterAction = (qty: number): ActionType => ({
   type: INCREMENT,
   qty
 });
 
-export const decrementCounterAction = (qty: number): any => ({
+export const decrementCounterAction = (qty: number): ActionType => ({
   type: DECREMENT,
   qty
 });
 
-const incrementReducer = (state: any, action: any): number => {
-  return state + 1;
+// Reducers shouldn't mutate objects, I kept like this for simplicity
+const incrementReducer = (state: StateSliceType, action: ActionType): StateSliceType => {
+  const interactor = new CounterInteractor(state);
+  interactor.increment(action.qty);
+  return state;
 };
 
-const decrementReducer = (state: any, action: any): number => {
-  return state + 1;
+// Reducers shouldn't mutate objects, I kept like this for simplicity
+const decrementReducer = (state: StateSliceType, action: ActionType): StateSliceType => {
+  const interactor = new CounterInteractor(state);
+  interactor.decrement(action.qty);
+  return state;
 };
 
 export const counterReducer = (
-  state: number = INITIAL_STATE,
-  action: any,
-): any => {
+  state: StateSliceType = INITIAL_STATE,
+  action: ActionType,
+): StateSliceType => {
   switch (action.type) {
     case INCREMENT:
       return incrementReducer(state, action);
--- core/lib/frameworks/redux.ts
+++ core/lib/frameworks/redux.ts
@@ -1,6 +1,11 @@
 import { combineReducers, applyMiddleware, createStore } from 'redux';
 import { createLogger } from 'redux-logger';
 import { counterReducer } from '../adapters/redux';
+import { Counter } from "../entities";
+
+export type StateType = {
+  counter: Counter,
+};
 
 const reducers = {
   counter: counterReducer,
