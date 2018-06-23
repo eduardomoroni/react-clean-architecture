a9e22ccce4604ce5f4cd345dbdb81bdf1fd5103a
Eduardo Moroni <eduardomoroni@gmail.com>
2018-06-23 09:10:15 -0300

    feat: add counter on native

---
+++ native/src/components/AppPresenter.tsx
@@ -0,0 +1,35 @@
+import {Counter, counterSelector, decrementCounterAction, incrementCounterAction, StateType} from "core"
+import * as React from 'react';
+import {connect} from "react-redux";
+import {Header} from "./Header";
+import {CounterComponent} from "./Counter";
+import {AppWrapper} from "./AppWrapper";
+
+interface IProps {
+  counter: Counter,
+  decrement: (qty: number) => void,
+  increment: (qty: number) => void,
+}
+
+export const AppModel = (props: IProps) => {
+  const increment = () => props.increment(1);
+  const decrement = () => props.decrement(1);
+
+  return (
+    <AppWrapper>
+      <Header />
+      <CounterComponent decrement={decrement} increment={increment} counter={props.counter.count}/>
+    </AppWrapper>
+  );
+};
+
+const mapStateToProps = (state: StateType) => ({
+  counter: counterSelector(state),
+});
+
+const mapDispatchToProps = {
+  decrement: decrementCounterAction,
+  increment: incrementCounterAction,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AppModel);
---
+++ native/src/components/Header.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import {Text, View} from "react-native";
+import {styles} from "../stylesheets/styles";
+
+export const Header = () => (
+  <Text style={styles.welcome}>Welcome to React Native</Text>
+);
